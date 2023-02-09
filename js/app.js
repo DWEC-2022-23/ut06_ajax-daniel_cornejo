3f89331bf62a624f0dff69640c77ab39ba56c47a
DainelCornejoStona <danicornejof@gmail.com>
2023-02-09 10:20:34 +0100

    push

--- js/app.js
+++ js/app.js
@@ -8,6 +8,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const div = document.createElement('div');
   const filterLabel = document.createElement('label');
   const filterCheckBox = document.createElement('input');
+
+  
+  function getInvitados(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:3000/invitados');
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        const invitados = JSON.parse(xhr.responseText);
+        invitados.forEach(invitado => {
+          const li = createLI(invitado.nombre);
+          ul.appendChild(li);
+        });
+      }
+    };  
+    xhr.send();
+  }
+  getInvitados();
   
   filterLabel.textContent = "Ocultar los que no hayan respondido";
   filterCheckBox.type = 'checkbox';
@@ -33,6 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }                                 
     }
   });
+
   
   function createLI(text) {
     function createElement(elementName, property, value) {
@@ -57,15 +75,28 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   
   form.addEventListener('submit', (e) => {
+    function addInvitado(nombre,confirmado) {
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', 'http://localhost:3000/invitados');
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 201) {
+          console.log('Invitado agregado');
+        }
+      };
+      xhr.send(JSON.stringify({ nombre, confirmado}));
+    }
+    
     e.preventDefault();
     const text = input.value;
     input.value = '';
     const li = createLI(text);
     ul.appendChild(li);
+    addInvitado(text,false);
   });
     
   ul.addEventListener('change', (e) => {
-    const checkbox = event.target;
+    const checkbox = e.target;
     const checked = checkbox.checked;
     const listItem = checkbox.parentNode.parentNode;
     
@@ -80,10 +111,23 @@ document.addEventListener('DOMContentLoaded', () => {
     if (e.target.tagName === 'BUTTON') {
       const button = e.target;
       const li = button.parentNode;
+      const id= li.getAttribute("id");
       const ul = li.parentNode;
       const action = button.textContent;
       const nameActions = {
         remove: () => {
+          function deleteInvitado(id) {
+            const xhr = new XMLHttpRequest();
+            xhr.open('DELETE', `http://localhost:3000/invitados/${id}`,true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onreadystatechange = function() {
+              if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                console.log('Invitado eliminado');
+              }
+            };
+            xhr.send(JSON.stringify({ id }));
+          }
+          deleteInvitado(id);
           ul.removeChild(li);
         },
         edit: () => {
@@ -96,12 +140,25 @@ document.addEventListener('DOMContentLoaded', () => {
           button.textContent = 'save';  
         },
         save: () => {
+          function updateInvitado(id, nombre) {
+            const xhr = new XMLHttpRequest();
+            xhr.open('PUT', `http://localhost:3000/invitados/${id}`);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onreadystatechange = function() {
+              if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                console.log('Invitado actualizado');
+              }
+            };
+            xhr.send(JSON.stringify({ nombre }));
+          }
+
           const input = li.firstElementChild;
           const span = document.createElement('span');
           span.textContent = input.value;
           li.insertBefore(span, input);
           li.removeChild(input);
-          button.textContent = 'edit';        
+          button.textContent = 'edit';    
+          updateInvitado(id,input.value);    
         }
       };
       
@@ -110,12 +167,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });  
 });  
-  
-  
-  
-  
-  
-  
-  
-  
-  
